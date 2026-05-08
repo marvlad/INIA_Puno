da0f0892248f00e283a56d206f7af1e28973ae1f
Marvin V. Ascencio S. <marvlad@users.noreply.github.com>
2026-05-08 11:13:59 -0500

    Add Waitress server for app deployment

--- INIA_Software/app.py
+++ INIA_Software/app.py
@@ -127,8 +127,16 @@ def generate():
 
 
 if __name__ == "__main__":
+    ```
     app.run(
         host="127.0.0.1",
         port=5000,
         debug=True,
+    )
+    ```
+    from waitress import serve
+    serve(
+        app,
+        host="127.0.0.1",
+        port=5000,
     )
