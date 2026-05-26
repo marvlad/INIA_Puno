a06d1fbce3c62e0413c9200c54dcae164d806f33
Marvin V. Ascencio S. <marvin.ascencios@gmail.com>
2026-05-26 10:33:01 -0500

    Merge load_db changes

--- NoExcel/load_db.py
+++ NoExcel/load_db.py
@@ -12,13 +12,8 @@ from openpyxl import load_workbook
 # Configuration
 # ------------------------------------------------------------
 
-<<<<<<< Updated upstream
-INPUT_EXCEL = "RESULTADOS USUARIOS 2M_Illpa_2.0.xlsx"
-SHEET_NAME = "BD2024"
-=======
 INPUT_EXCEL = "RESULTADOS USUARIOS 2M_Illpa 2.0.2_CORREGIDO.xlsx"
-SHEET_NAME = "Base_Datos"
->>>>>>> Stashed changes
+SHEET_NAME = "BD2024"
 
 OUTPUT_DB = "database/inia_database.sqlite"
 TABLE_NAME = "muestras"
